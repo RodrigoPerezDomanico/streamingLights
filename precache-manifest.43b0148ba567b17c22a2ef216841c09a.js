4f5b07a909f92818acae2797ddd2869db7a2b51d
Rodrigo Perez Domanico <rodrigoperez33@hotmail.com>
2020-09-07 18:43:37 -0300

    Updates

---
+++ precache-manifest.43b0148ba567b17c22a2ef216841c09a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b04a5fe409667d83e97215e4460507",
+    "url": "/streamingLights/index.html"
+  },
+  {
+    "revision": "fcf5bad72c1086fca0f0",
+    "url": "/streamingLights/static/css/main.e5df0dc0.chunk.css"
+  },
+  {
+    "revision": "2fdc8ca1fe27b489a2ad",
+    "url": "/streamingLights/static/js/2.23784c08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/streamingLights/static/js/2.23784c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf5bad72c1086fca0f0",
+    "url": "/streamingLights/static/js/main.780c0781.chunk.js"
+  },
+  {
+    "revision": "87daa093918ab8f2c4d2",
+    "url": "/streamingLights/static/js/runtime-main.445390a2.js"
+  }
+]);
